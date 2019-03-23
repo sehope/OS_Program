784a4a04e6089240e16aed30dcd2ac04f90d8a9a
Shelby Hope <sehope42@students.tntech.edu>
2019-03-23 17:37:00 -0500

    os added 2 bootloader.

--- bootloader.asm
+++ bootloader.asm
@@ -91,9 +91,41 @@ halt:
 greeting2:
 	db "Hello from Protected Mode ! :^)"
 
-call 0x8000 						; kernel entry point
+jmp _start			; kernel entry point
 
 ; ---
 
 TIMES 510 - ($-$$) db 0				; make sure length is 512 bytes
 DW 0xAA55							; boot signature
+
+
+
+
+
+
+
+
+; ---- OS PART ----
+
+section .data
+Text DB "Welcome to our first Operating System!", 0xa
+len  equ $-Text
+
+section .text
+	global _start
+
+_start:
+
+	mov edx, len
+	mov ecx, Text
+	mov ebx, 1
+	mov eax, edx
+	int 0x80
+
+	xor ah, ah
+	mov ax, 13h
+	int 10h
+
+	mov eax, 1
+	xor ebx, ebx
+	int 0x80

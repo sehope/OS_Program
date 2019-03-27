c82fb324204d730675bdb8eb8427734ef00d6b69
Miranda Plunkett <mgplunkett42@students.tntech.edu>
2019-03-27 23:25:08 +0000

    Bootloader with Input stuff

--- bootloader.asm
+++ bootloader.asm
@@ -48,10 +48,13 @@ start:
 	mov si, text
 	call print_string
 	jmp $
-	text db 'This is our first Operating System!', 0
-	
+	text db 'This is our first Operating System!', 0xa, 0xd, 0
+	text_user db 'S', 0
+	text_pass db 'M', 0
+
 print_string:
 	mov ah, 0Eh
+	mov bl, 0x3C
 
 .repeat:
 	lodsb
@@ -60,8 +63,109 @@ print_string:
 	int 10h
 	jmp .repeat
 
-.done: 
+.done:
+
+	mov ah, 00h
+	int 16h
+	cmp al, 13
+	jne .done
+
+call cls
+jmp $
+
+cls: 
+	pusha 
+	mov ah, 0x00
+	mov al, 0x03
+	int 0x10
+	popa
+
+mov si, text_2
+call print_string_2
+jmp $
+text_2 db 'user name: ', 0
+	
+print_string_2:
+	mov ah, 0Eh
+
+.repeat_2:
+	lodsb
+	cmp al, 0
+	je .done_2
+	int 10h
+	jmp .repeat_2
+
+.done_2: 
+	mov ah, 00h
+	int 16h
+	cmp al, 83
+	jne .done_2
+
+mov si, text_crnl
+call print_newline
+jmp$
+text_crnl db ' ', 0xa, 0xd, 0
+
+print_newline:
+	mov ah, 0Eh
+
+.repeat_newline:
+	lodsb
+	cmp al, 0
+	je .done_3
+	int 10h
+	jmp .repeat_newline
+
+.done_3:
+mov si, text_3
+call print_string_3
+jmp $
+text_3 db 'password: ', 0
+	
+print_string_3:
+	mov ah, 0Eh
+
+.repeat_3:
+	lodsb
+	cmp al, 0
+	je .done_4
+	int 10h
+	jmp .repeat_3
+
+.done_4:
+	mov ah, 00h
+	int 16h
+	cmp al, 77
+	jne .done_4
+
+call cls_2
+jmp $
+
+cls_2: 
+	pusha 
+	mov ah, 0x00
+	mov al, 0x03
+	int 0x10
+	popa
+
+mov si, text_5
+call print_string_5
+jmp $
+text_5 db 'Primes:  ', 0xa, 0xd,  0
+	
+print_string_5:
+	mov ah, 0Eh
+
+.repeat_5:
+	lodsb
+	cmp al, 0
+	je .done_5
+	int 10h
+	jmp .repeat_5
+.done_5:
 	ret
+
+
 
 TIMES 510 - ($-$$) db 0				; make sure length is 512 bytes
 DW 0xAA55							; boot signature
